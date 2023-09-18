fcc07e222baa79039e48d436bb0f3223dbbc70d1
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-09-19 00:13:34 +0330

    feat:add-section-divider

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,7 +89,7 @@ export default function Intro() {
         <Link
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-black"
           href="/resume.pdf"
-          download
+          target="_blank"
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
